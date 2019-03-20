79cf9a905ff8e33c9660d2e09f519891f6a5ce7c
LarryLeo <esharemore@gmail.com>
2019-03-20 17:21:10 +0800

    配置eslint和prettier

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -13,6 +13,11 @@ export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  methods: {
+    doSomeThing() {
+      console.log('打印写东西出来')
+    }
   }
 }
 </script>
