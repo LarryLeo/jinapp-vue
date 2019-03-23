bb976602d08947c9e2144d04ca5528db8e9fc8f9
LarryLeo <esharemore@gmail.com>
2019-03-23 18:28:49 +0800

    列表下拉和上滑加载实现

--- src/views/Notice.vue
+++ src/views/Notice.vue
@@ -1,9 +1,68 @@
 <template>
-  <div class="notice">政策宣传</div>
+  <div class="notice">
+    <van-pull-refresh v-model="refreshing" @refresh="fetchNotice('refresh')">
+      <van-list
+        @load="fetchNotice"
+        :finished="noMore"
+        finished-text="没有更多啦"
+      >
+        <van-cell v-for="(item, index) in noticeList" :key="index">
+          <div class="item">
+            <img src="../assets/images/notice-icon.jpg" alt="" class="icon" />
+            <p class="title">{{ item.title }}</p>
+          </div>
+        </van-cell>
+      </van-list>
+    </van-pull-refresh>
+  </div>
 </template>
 
 <script>
-export default {}
+import Vue from 'vue'
+import { List, Cell, PullRefresh } from 'vant'
+import { requestGet } from '../utils/index'
+
+Vue.use(List)
+  .use(Cell)
+  .use(PullRefresh)
+export default {
+  name: 'Notice',
+  data() {
+    return {
+      noticeList: [],
+      pn: 1,
+      ps: 10,
+      loading: false,
+      refreshing: false,
+      noMore: false
+    }
+  },
+  created() {
+    this.fetchNotice()
+  },
+  methods: {
+    async fetchNotice(refresh) {
+      if (refresh) {
+        this.noMore = false
+        this.noticeList = []
+      }
+      if (this.noMore || this.loading)
+        return console.log('没有更多数据了, 或加载中')
+      this.loading = true
+      let res = await requestGet('/app/v1/index/articleList', {
+        type_id: 3,
+        pn: this.pn,
+        ps: this.ps
+      })
+      let list = res.data.list
+      this.noticeList = [...this.noticeList, ...list]
+      this.pn = list.length < this.ps ? this.pn : ++this.pn
+      this.noMore = list.length < this.ps
+      this.loading = false
+      this.refreshing = false
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -14,5 +73,20 @@ export default {}
   top: 0;
   left: 0;
   width: 100%;
+  overflow: auto;
+}
+.item {
+  display: flex;
+  .icon {
+    margin-right: 20px;
+    width: 50px;
+    height: 50px;
+  }
+  .title {
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    width: 250px;
+  }
 }
 </style>
